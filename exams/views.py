560d5769efad5a7cef0e0c38cd9a37d69db4ff42
Grant Bellar <grantbellar@Grants-Laptop.local>
2024-01-20 11:05:39 -0600

    Successfully pass question id on post request. Print user choice to console

--- exams/views.py
+++ exams/views.py
@@ -5,12 +5,17 @@ from .models import *
 def question_view(request, pk):
     current_question = Question.objects.get(id=pk)
     context = {"question": current_question}
+    
     if request.method == 'GET':
         return render(request, 'exams/question.html', context)
+    
     if request.method == 'POST':
         print(request.POST.get('user_answer'))
+        pk +=1
+        next_question = Question.objects.get(id=pk)
+        context['question'] = next_question
+        return render(request, 'exams/question.html', context)
 
 
-
-# TODO: Can I use a DetailView and FormView together? Where do I send the form data? Work on how to
-    # submit form answers and get next question.
+# Need a way better way to store which choice a user made. Right now there might be 500 choices and a 
+    # choice would be Choice23 which doesn't tell me which question is belongs to.
