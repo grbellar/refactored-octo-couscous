95d2b72492e0b6ee1d6ddafcf9b00962f679c4d9
Grant Bellar <grantbellar@Grants-Laptop.local>
2024-01-20 13:12:01 -0600

    Check for correct answer and add feedback

--- exams/views.py
+++ exams/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import *
 
-
+            
 def question_view(request, pk):
     current_question = Question.objects.get(id=pk)
     context = {"question": current_question}
@@ -10,12 +10,16 @@ def question_view(request, pk):
         return render(request, 'exams/question.html', context)
     
     if request.method == 'POST':
-        print(request.POST.get('user_answer'))
+        user_choice = request.POST.get('user_choice')
+        is_user_choice_correct = Choice.objects.get(id=user_choice).is_correct
+        print(user_choice)
+        print(is_user_choice_correct)
         pk +=1
         next_question = Question.objects.get(id=pk)
         context['question'] = next_question
+        context['is_correct'] = is_user_choice_correct
         return render(request, 'exams/question.html', context)
 
 
 # Need a way better way to store which choice a user made. Right now there might be 500 choices and a 
-    # choice would be Choice23 which doesn't tell me which question is belongs to.
+    # choice would be Choice23 which doesn't tell me which question it belongs to.
