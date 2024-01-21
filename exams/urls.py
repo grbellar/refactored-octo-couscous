db368cd4fff1489af1a43305638f5fadc2916a13
Grant Bellar <grantbellar@Grants-Laptop.local>
2024-01-20 18:17:42 -0600

    WIP

--- exams/urls.py
+++ exams/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import question_view, take_exam_view
+from .views import take_exam_view
 
 urlpatterns = [
-    path("exams/question/<int:pk>", question_view, name='question'),
+    # path("exams/question/<int:pk>", question_view, name='question')
     path("exams/exam/<int:pk>", take_exam_view, name='take-exam')
 ]
--- exams/views.py
+++ exams/views.py
@@ -1,27 +1,26 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
-from exams.models import Exam
 from .models import *
 
 
-@require_http_methods(['GET', 'POST'])       
-def question_view(request, pk):
-    current_question = Question.objects.get(id=pk)
-    context = {"question": current_question}
+# @require_http_methods(['GET', 'POST'])       
+# def question_view(request, pk):
+#     current_question = Question.objects.get(id=pk)
+#     context = {"question": current_question}
     
-    if request.method == 'GET':
-        return render(request, 'exams/question.html', context)
+#     if request.method == 'GET':
+#         return render(request, 'exams/question.html', context)
     
-    if request.method == 'POST':
-        user_choice = request.POST.get('user_choice')
-        is_user_choice_correct = Choice.objects.get(id=user_choice).is_correct
-        print(user_choice)
-        print(is_user_choice_correct)
-        pk +=1
-        next_question = Question.objects.get(id=pk)
-        context['question'] = next_question
-        context['is_correct'] = is_user_choice_correct
-        return render(request, 'exams/question.html', context)
+#     if request.method == 'POST':
+#         user_choice = request.POST.get('user_choice')
+#         is_user_choice_correct = Choice.objects.get(id=user_choice).is_correct
+#         print(user_choice)
+#         print(is_user_choice_correct)
+#         pk +=1
+#         next_question = Question.objects.get(id=pk)
+#         context['question'] = next_question
+#         context['is_correct'] = is_user_choice_correct
+#         return render(request, 'exams/question.html', context)
 
 
 def take_exam_view(request, pk):
@@ -29,8 +28,24 @@ def take_exam_view(request, pk):
     # comments on one approach with giving each user an Exam.
     #TODO: See take_exam.html
     exam = Exam.objects.get(id=pk)
-    context = {"exam": exam}
-    return render(request, "exams/take_exam.html", context)
+    exam_questions = exam.question.all()
+
+    first_question = exam_questions.first()
+    current_question = 1
+    context = {"exam": exam,
+               "question": first_question}    # TODO: Surely I can reverse this query and get the exam name from question without the need to pass exam.
+
+    
+    if request.method == 'GET':
+        return render(request, "exams/take_exam.html", context)
+    if request.method == 'POST':
+        user_choice = request.POST.get('user_choice')
+        is_user_choice_correct = Choice.objects.get(id=user_choice).is_correct
+        current_question +=1
+        next_question = exam_questions.get(id=current_question)
+        context['question'] = next_question
+        context['is_correct'] = is_user_choice_correct
+        return render(request, 'exams/question.html', context)
 
 
 # Need a way better way to store which choice a user made. Right now there might be 500 choices and a 
