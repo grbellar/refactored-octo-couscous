3ed3a0ee83b0014ddd8e5d4a2fe4cfa48b378c33
Grant Bellar <grantbellar@Grants-Laptop.local>
2024-01-20 20:06:43 -0600

    Code cleanup and TODOs. Remove question.html
    Also delete associated views and urls as this functionaly has been brough into take_exam view and expanded upon. Woo.

--- exams/urls.py
+++ exams/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import take_exam_view
 
 urlpatterns = [
-    # path("exams/question/<int:pk>", question_view, name='question')
     path("exams/exam/<int:pk>", take_exam_view, name='take-exam')
 ]
--- exams/views.py
+++ exams/views.py
@@ -3,26 +3,7 @@ from django.views.decorators.http import require_http_methods
 from .models import *
 
 
-# @require_http_methods(['GET', 'POST'])       
-# def question_view(request, pk):
-#     current_question = Question.objects.get(id=pk)
-#     context = {"question": current_question}
-    
-#     if request.method == 'GET':
-#         return render(request, 'exams/question.html', context)
-    
-#     if request.method == 'POST':
-#         user_choice = request.POST.get('user_choice')
-#         is_user_choice_correct = Choice.objects.get(id=user_choice).is_correct
-#         print(user_choice)
-#         print(is_user_choice_correct)
-#         pk +=1
-#         next_question = Question.objects.get(id=pk)
-#         context['question'] = next_question
-#         context['is_correct'] = is_user_choice_correct
-#         return render(request, 'exams/question.html', context)
-
-
+@require_http_methods(['GET', 'POST'])  
 def take_exam_view(request, pk):
 
     exam = Exam.objects.get(id=pk)
@@ -37,7 +18,7 @@ def take_exam_view(request, pk):
     
     if user_progress.completed:
         print('Exam complete')
-        pass # handle exam completion by return somewhere else. The way this is written it will cause an index of range error
+        pass # TODO: handle exam completion by return somewhere else. The way this is written it will cause an index of range error
             # for current_question_index below if not. May need to refactor this as CHAT may not have the best ideas.
 
     current_question_index = user_progress.current_question_index
@@ -49,7 +30,7 @@ def take_exam_view(request, pk):
     
     if request.method == 'POST':
         user_choice = request.POST.get('user_choice')
-        # store user choice
+        # TODO: store user choice
         
         current_question_index +=1
 
@@ -68,24 +49,6 @@ def take_exam_view(request, pk):
             context['question'] = next_question
 
     return render(request, "exams/take_exam.html", context)
-
-        
-
-
-
-
-
-    
-    # if request.method == 'GET':
-    #     return render(request, "exams/take_exam.html", context)
-    # if request.method == 'POST':
-    #     user_choice = request.POST.get('user_choice')
-    #     is_user_choice_correct = Choice.objects.get(id=user_choice).is_correct
-    #     current_question +=1
-    #     next_question = exam_questions.get(id=current_question)
-    #     context['question'] = next_question
-    #     context['is_correct'] = is_user_choice_correct
-    #     return render(request, 'exams/question.html', context)
 
 
 # Need a way better way to store which choice a user made. Right now there might be 500 choices and a 
