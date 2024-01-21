abfc517e4d07759d4cf33d1b3dbe64e3ac73aaf7
Grant Bellar <grantbellar@Grants-Laptop.local>
2024-01-20 19:27:27 -0600

    Store and retrieve user progress in exam view

--- exams/models.py
+++ exams/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 
 class Exam(models.Model):
@@ -27,6 +28,21 @@ class Choice(models.Model):
     question = models.ForeignKey("Question", on_delete=models.CASCADE, related_name="choices")
     text = models.TextField(null=False, blank=False)
     is_correct = models.BooleanField()
+
+
+class UserExamProgress(models.Model):
+    exam = models.ForeignKey('Exam', on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING)
+    current_question_index = models.IntegerField(default=0)
+    completed = models.BooleanField(default=False)
+
+
+    class Meta:
+        unique_together = ('user', 'exam')  # Ensure one entry per user per exam
+
+
+
+
 
 # TODO: 2. Build correct/incorrect functionality so I can test that. Need to figure out what relationships are needed
     # and whether they are working correctly together.
--- exams/views.py
+++ exams/views.py
@@ -24,28 +24,67 @@ from .models import *
 
 
 def take_exam_view(request, pk):
-    # Need to figure out how to make sure I am storing user specific exam data as they begin taking this exam. See previous commit for 
-    # comments on one approach with giving each user an Exam.
-    #TODO: See take_exam.html
+
     exam = Exam.objects.get(id=pk)
-    exam_questions = exam.question.all()
+    exam_questions = list(exam.question.all()) # wrapping this in a list to make this easier to work with
 
-    first_question = exam_questions.first()
-    current_question = 1
-    context = {"exam": exam,
-               "question": first_question}    # TODO: Surely I can reverse this query and get the exam name from question without the need to pass exam.
-
+    # Get UserExamProgress instance or create one if it doesn't exist.
+    user_progress, created = UserExamProgress.objects.get_or_create(
+        user=request.user,
+        exam=exam,
+    )
     
-    if request.method == 'GET':
-        return render(request, "exams/take_exam.html", context)
+    if user_progress.completed:
+        print('Exam complete')
+        pass # handle exam completion by return somewhere else. The way this is written it will cause an index of range error
+            # for current_question_index below if not. May need to refactor this as CHAT may not have the best ideas.
+
+    current_question_index = user_progress.current_question_index
+    print(current_question_index)
+    current_question = exam_questions[current_question_index]
+
+    context = {"exam": exam,
+               "question": current_question}
+    
     if request.method == 'POST':
         user_choice = request.POST.get('user_choice')
-        is_user_choice_correct = Choice.objects.get(id=user_choice).is_correct
-        current_question +=1
-        next_question = exam_questions.get(id=current_question)
-        context['question'] = next_question
-        context['is_correct'] = is_user_choice_correct
-        return render(request, 'exams/question.html', context)
+        # store user choice
+        
+        current_question_index +=1
+
+        if current_question_index >= len(exam_questions):
+            user_progress.completed = True
+        
+        user_progress.current_question_index = current_question_index
+        user_progress.save()
+        
+        if user_progress.completed:
+            # redirect to compeltion page. 
+            print('Exam complete.')
+            pass
+        else:
+            next_question = exam_questions[user_progress.current_question_index]
+            context['question'] = next_question
+
+    return render(request, "exams/take_exam.html", context)
+
+        
+
+
+
+
+
+    
+    # if request.method == 'GET':
+    #     return render(request, "exams/take_exam.html", context)
+    # if request.method == 'POST':
+    #     user_choice = request.POST.get('user_choice')
+    #     is_user_choice_correct = Choice.objects.get(id=user_choice).is_correct
+    #     current_question +=1
+    #     next_question = exam_questions.get(id=current_question)
+    #     context['question'] = next_question
+    #     context['is_correct'] = is_user_choice_correct
+    #     return render(request, 'exams/question.html', context)
 
 
 # Need a way better way to store which choice a user made. Right now there might be 500 choices and a 
