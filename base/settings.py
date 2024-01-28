1fa0ad9592f09b1bc79d07f8fb6c767864dfe21d
Grant Bellar <grantbellar@Grants-Laptop.local>
2024-01-28 11:08:04 -0600

    Swap sqlite3 adapter for postgres

--- base/settings.py
+++ base/settings.py
@@ -75,8 +75,12 @@ TEMPLATES = [
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "grantbellar",
+        "USER": "grantbellar",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
