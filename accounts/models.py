5df9e87463c53f31d8f8153788f21970c1a940c3
Grant Bellar <grantbellar@Grants-Laptop.local>
2024-01-28 08:57:47 -0600

    WIP

--- accounts/models.py
+++ accounts/models.py
@@ -9,11 +9,5 @@ class CustomUser(AbstractUser):
 
     #TODO: Add a UUID field so that I can pass that to stripe and use that to identify users. Better than using sequential integers
 
-    def check_with_stripe():
-        # Not sure if I'll need this. Stubbing for now. Return True or False if paid.
-        pass
-
-
-# Note sure what this is for as the display is defined in the CustomUserAdmin class (admin.py).
     def __str__(self):
         return self.email
