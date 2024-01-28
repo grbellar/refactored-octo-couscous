f204931483bc62de7267bdf96dc0c4a8a1272185
Grant Bellar <grantbellar@Grants-Laptop.local>
2024-01-28 11:35:08 -0600

    Add uuid field for all users

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,10 @@
+import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from exams.models import Exam
 
 class CustomUser(AbstractUser):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True) # Using this for url parameters    
     has_paid = models.BooleanField(default=False, null=False)
     exam = models.ManyToManyField(Exam)
     #TODO: Figure out why my customer fields aren't showing up in the Django User admin panel.
