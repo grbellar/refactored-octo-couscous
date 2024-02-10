05b09dc433975726e12d246240ed0de79577622d
Grant Bellar <grantbellar@Grants-Laptop.local>
2024-02-10 11:56:58 -0600

    Add exam_token field for all users

--- accounts/models.py
+++ accounts/models.py
@@ -6,11 +6,11 @@ from exams.models import Exam
 class CustomUser(AbstractUser):
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True) # Using this for url parameters    
     has_paid = models.BooleanField(default=False, null=False)
+    exam_tokens = models.IntegerField(default=0, null=False, blank=False) # Maybe this could be its own model?
     exam = models.ManyToManyField(Exam)
     school = models.CharField(max_length=300, null=True, blank=True)
+    
+    
     #TODO: Figure out why my customer fields aren't showing up in the Django User admin panel.
-
-    #TODO: Add a UUID field so that I can pass that to stripe and use that to identify users. Better than using sequential integers
-
     def __str__(self):
         return self.email
