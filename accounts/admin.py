7aa5b7284e7f3fd03cddaf6c92cd2238c61ac9ab
Grant Bellar <grbellar@gmail.com>
2024-03-30 11:46:41 -0500

    Beef up CustomUser admin with paid info; hide unnecessary info

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,14 +1,43 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
-
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
+from exams.admin import UserExamState
+
+class UserExamStateInline(admin.StackedInline):
+    model = UserExamState
+    extra = 0
+
+    can_delete = False
+
+    def get_readonly_fields(self, request, obj=None):
+        # Make all fields in the UserExamState inline read-only
+        return [field.name for field in self.model._meta.fields]
+
 
 class CustomUserAdmin(UserAdmin):
+    inlines = [UserExamStateInline]
+
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email', 'first_name', 'last_name', 'username',]
+    
+    list_display = ['email', 'first_name', 'last_name',]
+    readonly_fields = ['uuid', 'date_joined', 'last_login']
+
+    fieldsets = [
+        (
+            'User Information',
+            {
+                "fields": ["first_name", "last_name", "email", "school", "date_joined", "uuid", "password"]
+            }
+        ),
+        (
+            'Paid Status',
+            {
+                "fields": ["has_paid", "exam_tokens"]
+            }
+        ),
+    ]
 
 admin.site.register(CustomUser, CustomUserAdmin)
