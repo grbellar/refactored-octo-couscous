43f183011fbaf7947f4bc0b83b373b4288f3507f
Grant Bellar <grbellar@gmail.com>
2024-10-25 12:11:13 -0500

    Test quiz stuff

--- review/views.py
+++ review/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+import json
+
+with open('review/test-review-data.json', 'r') as file:
+    quizzes = json.load(file)
 
 # Create your views here.
 def review(request):
+    print(quizzes)
+    for question in quizzes:
+        print(question["text"])
     return render(request, "review/review.html")
 
