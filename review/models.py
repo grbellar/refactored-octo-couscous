59b9d7a0ef05a98bcff28f17d827cd37c8eba2a6
Grant Bellar <grbellar@gmail.com>
2024-12-21 10:41:01 -0600

    Create explanation Model and add sentitel quiz for QuizResult
    Adding sentinel quiz in case quiz is deleted but people have results already. Also using json field to store the sources in for the explanation object.

--- review/models.py
+++ review/models.py
@@ -27,7 +27,6 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     text = models.TextField()
     is_correct = models.BooleanField(default=False)
-    explanation = models.TextField(null=True, blank=True)
     choice_count = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -35,12 +34,20 @@ class Answer(models.Model):
     def __str__(self):
         return self.text
     
-    # apparently 'sentinel' user is a common pattern to handle data integrity when a user is deleted.
+class Explanation(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.TextField()
+    sources = models.JSONField(default=list, blank=True)
+    
 def get_sentinel_user():
     return get_user_model().objects.get_or_create(username='deleted')[0]
 
+def get_sentinel_quiz(): 
+    return Quiz.objects.get_or_create(title="Deleted Quiz", description="This quiz has been deleted")[0]
+
 class QuizResult(models.Model):
-    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Quiz, on_delete=models.SET(get_sentinel_quiz))
+    quiz_title_snapshot = models.CharField(blank=True)
     user = models.ForeignKey(
         AUTH_USER_MODEL, 
         on_delete=models.SET(get_sentinel_user),
@@ -49,6 +56,11 @@ class QuizResult(models.Model):
     score = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if self.quiz and not self.quiz_title_snapshot:
+            self.quiz_title_snapshot = self.quiz.title
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.user.username} - {self.quiz.title}"
