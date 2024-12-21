6e422f5e60c207c16d15ebcf5ad9dc22e9ac9ba4
Grant Bellar <grbellar@gmail.com>
2024-12-21 09:40:07 -0600

    Import django settings so I can run import script

--- insert_data.py
+++ insert_data.py
@@ -1,7 +1,15 @@
+import os
+import django
+
+# Set the settings module for your Django project
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
+
+# Initialize Django
+django.setup()
+
+
 from exams.models import Category, Question, Choice
 import json
-
-
 with open('data.json', 'r') as file:
     question_list = json.load(file)
 
