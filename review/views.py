74466b5dfa004b8cdeb2a4d8aefa41a4e232bca2
Grant Bellar <grbellar@gmail.com>
2025-01-14 15:46:27 -0600

    Major WIP changes

--- review/views.py
+++ review/views.py
@@ -14,14 +14,7 @@ def get_quiz_question(quiz_uuid, question_index):
     quiz = Quiz.objects.get(uuid=quiz_uuid)
     print(quiz.question_set.all())
     quiz_question = quiz.question_set.all()[question_index]
-    
-    # Debug prints
-    print("Quiz Question ID:", quiz_question.id)
-    print("Quiz Question Fields:", vars(quiz_question))
-    print("Quiz Question Dir:", dir(quiz_question))
-    print("Explanation:", getattr(quiz_question, 'explanation', 'No explanation found'))
-    
-    pass
+    return quiz_question
 
 
 @login_required
@@ -30,7 +23,7 @@ def take_quiz(request, quiz_uuid): # TODO: this should become get_quiz prob. All
     quiz = Quiz.objects.get(uuid=quiz_uuid)
     user_quiz_state = UserQuizState.objects.get_or_create(user=request.user, quiz=quiz)[0]
     current_question = get_quiz_question(quiz_uuid, user_quiz_state.current_question_index)
-    
+    print(current_question)
     # Initial page load
     return render(request, "review/take_quiz.html", {'quiz': quiz, 'current_question': current_question})
 
@@ -39,25 +32,40 @@ def take_quiz(request, quiz_uuid): # TODO: this should become get_quiz prob. All
 @require_http_methods(['POST'])
 @requires_csrf_token
 def check_answer(request, quiz_uuid):
-
-    # TODO: I need to check for is_complete seperately of whether or not we are on the last question because use can go back and forth once 
-    # they have answered all the questions
     quiz = Quiz.objects.get(uuid=quiz_uuid)
     user_quiz_state = UserQuizState.objects.get(user=request.user, quiz=quiz)
     
-    if not user_quiz_state.completed:
-        # Get the submitted answer ID from the form
-        user_answer_id = request.POST.get('user_answer')
-        if not user_answer_id:
-            return JsonResponse({'error': 'No answer provided'}, status=400)
-            
-        # Get the selected answer and current question
-        selected_answer = Answer.objects.get(id=user_answer_id)
-        # TODO: get_quiz_question was supposed to be a way to fetch the next (or previous) question.
-        # TODO: Need to check if quiz has ended here.
-        current_question = get_quiz_question(quiz_uuid, user_quiz_state.current_question_index) # TODO: Should verify these indexes are accurate
+    button_clicked = request.POST.get('button')
+    current_question = get_quiz_question(quiz_uuid, user_quiz_state.current_question_index)
+
+    # Handle navigation in review mode
+    if user_quiz_state.completed:
+        if button_clicked == 'back-btn' and user_quiz_state.current_question_index > 0:
+            user_quiz_state.current_question_index -= 1
+        elif button_clicked == 'check-answer-btn' and user_quiz_state.current_question_index < quiz.question_set.count() - 1:
+            user_quiz_state.current_question_index += 1
+        user_quiz_state.save()
+        # TODO: Also update time stamp
         
-        # Save the user's answer
+        current_question = get_quiz_question(quiz_uuid, user_quiz_state.current_question_index)
+        # Get the user's previous answer for this question
+        saved_answer = UserQuizAnswer.objects.get(
+            user_quiz_state=user_quiz_state,
+            question=current_question
+        )
+        
+        return JsonResponse({
+            'is_correct': saved_answer.is_correct,
+            'explanation': current_question.explanation,
+            'is_complete': True,
+            'current_question_index': user_quiz_state.current_question_index,
+            'total_questions': quiz.question_set.count()
+        })
+
+    # TODO: current_question_index is not saving to db correcty
+    if button_clicked == 'check-answer-btn':
+        user_answer_id = request.POST.get('user_answer')
+        selected_answer = Answer.objects.get(id=user_answer_id)
         UserQuizAnswer.objects.create(
             user_quiz_state=user_quiz_state,
             question=current_question,
@@ -65,23 +73,58 @@ def check_answer(request, quiz_uuid):
             is_correct=selected_answer.is_correct
         )
         
-        # Update quiz state
-        if user_quiz_state.current_question_index == len(quiz.question_set.all()) - 1:
-            # Reached end of quiz.
-            print("Reached end of quiz.")
-        else:
+        return JsonResponse({
+            'is_correct': selected_answer.is_correct,
+            'explanation': current_question.explanation.text if hasattr(current_question.explanation, 'text') else current_question.explanation
+        })
+
+    if button_clicked == 'next-btn':    
+        is_last_question = user_quiz_state.current_question_index == quiz.question_set.count() - 1
+        
+        if not is_last_question:
             user_quiz_state.current_question_index += 1
             user_quiz_state.save()
+            next_question = get_quiz_question(quiz_uuid, user_quiz_state.current_question_index)
             
-        # Prepare response data
-        # TODO: Need to pass next or previous question as well if not complete
-        response_data = {
-            'is_correct': selected_answer.is_correct,
-            'explanation': current_question.explanation,
-            'is_complete': user_quiz_state.current_question_index >= quiz.question_set.count(), # TODO: imp better check here
-        }
+            return JsonResponse({
+                'next_question': {
+                    'text': next_question.text,
+                    'answers': list(next_question.answer_set.values('id', 'text'))
+                },
+                'is_complete': user_quiz_state.completed,
+                'current_question_index': user_quiz_state.current_question_index,
+                'total_questions': quiz.question_set.count()
+            })
+        else:
+            user_quiz_state.completed = True
+            user_quiz_state.save()
+            return JsonResponse({
+                'is_complete': True,
+                'message': 'Quiz completed'
+            })
+    elif button_clicked == 'back-btn':
+        if user_quiz_state.current_question_index > 0:
+            user_quiz_state.current_question_index -= 1
+            user_quiz_state.save()
+            
+        prev_question = get_quiz_question(quiz_uuid, user_quiz_state.current_question_index)
+        # Get the user's previous answer for this question
+        previous_answer = UserQuizAnswer.objects.get(
+            user_quiz_state=user_quiz_state,
+            question=prev_question
+        )
         
-        return JsonResponse(response_data)
-    else:
-        # Do something else. Maybe a serarate function to handle clicking through questions.
-        pass
+        return JsonResponse({
+            'next_question': {
+                'text': prev_question.text,
+                'answers': list(prev_question.answer_set.values('id', 'text'))
+            },
+            'previous_answer': {
+                'selected_answer_id': previous_answer.selected_answer.id,
+                'is_correct': previous_answer.is_correct,
+                'explanation': prev_question.explanation
+            },
+            'is_complete': user_quiz_state.completed,
+            'current_question_index': user_quiz_state.current_question_index,
+            'total_questions': quiz.question_set.count()
+        })
