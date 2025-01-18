178fa233cd80ff128b5d0b6bf4302a1c26ef23e8
Grant Bellar <grbellar@gmail.com>
2025-01-18 14:20:07 -0600

    Show UUID on quiz edit page

--- review/admin.py
+++ review/admin.py
@@ -7,9 +7,10 @@ admin.site.register(Question)
 admin.site.register(Answer)
 admin.site.register(UserQuizAnswer)
 admin.site.register(UserQuizState)
-
 class QuizAdmin(admin.ModelAdmin):
     filter_horizontal = ('questions',)
+    readonly_fields = ('uuid',)
+    list_display = ('title', 'uuid')
 
 admin.site.register(Quiz, QuizAdmin)
 
