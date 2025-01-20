3db343c70a0bb2105efbfdfb5ac2f123b0ed21b9
Grant Bellar <grbellar@gmail.com>
2025-01-19 23:01:26 -0600

    Create quiz script

--- createquizzes.py
+++ createquizzes.py
@@ -3,31 +3,37 @@ import django
 import random
 
 # Set the settings module for your Django project
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.local_settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
 
 # Initialize Django
 django.setup()
 
-from review.models import Question, Quiz
+from review.models import Quiz, Question
+from exams.models import Category
 
-# Fetch all questions
-all_questions = list(Question.objects.all())
+# Fetch all categories
+all_categories = Category.objects.all()
+print(all_categories)
 
-# Shuffle the questions to ensure randomness
-random.shuffle(all_questions)
+for category in all_categories:
+    # Fetch all questions for the current category
+    category_questions = list(Question.objects.filter(category=category))
 
-# Determine the number of quizzes to create
-num_quizzes = len(all_questions) // 10
+    # Shuffle the questions to ensure randomness
+    random.shuffle(category_questions)
 
-for i in range(num_quizzes):
-    # Create a new quiz
-    new_quiz = Quiz.objects.create(title=f"Anatomy and Physiology: Quiz{i+1}")
-    print(f"\nCreated {new_quiz.title}")
+    # Determine the number of quizzes to create for this category
+    num_quizzes = len(category_questions) // 10
 
-    # Assign 10 questions to the quiz
-    quiz_questions = all_questions[i*10:(i+1)*10]
-    for question in quiz_questions:
-        new_quiz.questions.add(question)
-        print(f"Added question: {question.text}")
+    for i in range(num_quizzes):
+        # Create a new quiz for the current category
+        new_quiz = Quiz.objects.create(title=f"{category.name}: Quiz {i+1}")
+        print(f"\nCreated {new_quiz.title}")
+
+        # Assign 10 questions to the quiz
+        quiz_questions = category_questions[i*10:(i+1)*10]
+        for question in quiz_questions:
+            new_quiz.questions.add(question)
+            print(f"Added question: {question.text}")
 
 print("\nDone.")
