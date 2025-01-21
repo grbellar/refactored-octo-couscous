21f4a0a147cf9136c747acc80a89865e7b8495a5
Grant Bellar <grbellar@gmail.com>
2025-01-21 10:39:40 -0600

    Review Admin improvements

--- review/admin.py
+++ review/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import *
+from django.utils.safestring import mark_safe
 
 # Register your models here.
 
@@ -8,8 +9,39 @@ class QuestionAdmin(admin.ModelAdmin):
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Answer)
-admin.site.register(UserQuizAnswer)
-admin.site.register(UserQuizState)
+
+class ExplanationAdmin(admin.ModelAdmin):
+    readonly_fields = ('question',)
+
+admin.site.register(Explanation, ExplanationAdmin)
+
+class UserQuizAnswerAdmin(admin.ModelAdmin):
+    def get_readonly_fields(self, request, obj=None):
+        # Make all fields read-only
+        fields = [field.name for field in self.model._meta.fields]
+        if obj:  # Only add explanation when editing an object
+            fields.append('question_explanation')
+        return fields
+        
+    def question_explanation(self, obj):
+        if obj.question and hasattr(obj.question, 'explanation'):
+            explanation = obj.question.explanation
+            # Return a link to the explanation in the admin
+            return mark_safe(f'<a href="/admin/review/explanation/{explanation.id}/change/">{explanation}</a>')
+        return "No explanation available"
+    
+    question_explanation.short_description = "Explanation"
+    
+    list_display = ('question',)
+
+admin.site.register(UserQuizAnswer, UserQuizAnswerAdmin)
+
+class UserQuizStateAdmin(admin.ModelAdmin):
+    def get_readonly_fields(self, request, obj=None):
+        # Make all fields read-only
+        return [field.name for field in self.model._meta.fields]
+
+admin.site.register(UserQuizState, UserQuizStateAdmin)
 class QuizAdmin(admin.ModelAdmin):
     filter_horizontal = ('questions',)
     readonly_fields = ('uuid',)
