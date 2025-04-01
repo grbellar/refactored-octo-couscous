372ca2bccdfaa6ad9c16dbdfa8d5a04ead4640eb
Grant Bellar <grbellar@gmail.com>
2025-04-01 17:21:39 -0500

    Fix countdown and add site header to admin

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,9 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "PerfusionBoardPrep.com"
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
